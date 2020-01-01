cd3b663c285afdbf5017c1551ed13710517bcb65
Nightcoder <newnightcoder@gmail.com>
2020-01-01 09:39:54 +0100

    DAY 18 - 1st of january 2020

---
+++ R1D18/index.js
@@ -0,0 +1,2 @@
+//my first js file ever haha!!
+alert('HAPPY NEW YEAR to my 100DaysOfCode friends, fellow learners, devs and coders! \n I AM GETTING INTRODUCED TO JS RIGHT NOW!!! YESSS!!!\n i love life, hahaha! :) \n 2020 will be GREAT! <3');
